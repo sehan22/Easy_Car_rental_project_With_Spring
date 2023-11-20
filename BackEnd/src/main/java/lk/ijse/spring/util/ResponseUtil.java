49a61d20f163ee8f180f2a2656ca743216a9971b
sehan22 <sehanranaweera2002@gmail.com>
2023-11-20 22:20:07 +0530

    Backend ResposeUtil Class updated.

--- BackEnd/src/main/java/lk/ijse/spring/util/ResponseUtil.java
+++ BackEnd/src/main/java/lk/ijse/spring/util/ResponseUtil.java
@@ -1,4 +1,14 @@
 package lk.ijse.spring.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ResponseUtil {
+    private String stat;
+    private String message;
+    private Object data;
 }
