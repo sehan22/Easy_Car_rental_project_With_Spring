1fa0fbccc8df78ed9f87e57976882bc1ca791903
sehan22 <sehanranaweera2002@gmail.com>
2023-11-21 23:15:57 +0530

    Backend Configure WebAppConfig.java

--- BackEnd/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ BackEnd/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -1,4 +1,29 @@
 package lk.ijse.spring.config;
 
-public class WebAppConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"lk.ijse.spring.controller", "lk.ijse.spring.advisor"})
+public class WebAppConfig implements WebMvcConfigurer {
+    public WebAppConfig() {
+        System.out.println("WebAppConfig : Instantiated");
+    }
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        return multipartResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**").addResourceLocations("/uploads/");
+    }
 }
